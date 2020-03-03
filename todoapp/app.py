d05c9049c48e3214262b6ccce0ff1cac55319aaa
ian douglas <ian.douglas@iandouglas.com>
2020-03-02 23:33:05 -0700

    migrations and checkbox endpoin

--- todoapp/app.py
+++ todoapp/app.py
@@ -1,4 +1,5 @@
-from flask import Flask, render_template, request, jsonify, abort
+from flask import Flask, render_template, request, jsonify, abort, redirect, url_for
+from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy
 import sys
 
@@ -7,23 +8,34 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql:///todoapp'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
-
+migrate = Migrate(app, db)
 
 class Todo(db.Model):
   __tablename__ = 'todos'
   id = db.Column(db.Integer, primary_key=True)
   description = db.Column(db.String, nullable=False)
+  completed = db.Column(db.Boolean, default=False, nullable=False)
 
   def __repr__(self):
-    return f'Todo: id:{self.id}, d:{self.description}'
+    return f'Todo: id:{self.id}, d:{self.description}, c:{self.completed}'
 
-
-db.create_all()
 
 
 @app.route('/')
 def index():
-  return render_template('index.html', data=Todo.query.all())
+  return render_template('index.html', data=Todo.query.order_by('id').all())
+
+@app.route('/todos/<todo_id>/set-completed', methods=['POST'])
+def change_todo(todo_id):
+  try:
+    todo = Todo.query.get(todo_id)
+    todo.completed = request.get_json()['completed']
+    db.session.commit()
+  except:
+    db.session.rollback()
+  finally:
+    db.session.close()
+  return redirect(url_for('index'))
 
 @app.route('/todos', methods=['POST'])
 def create_todo():
