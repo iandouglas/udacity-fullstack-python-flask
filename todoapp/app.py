8ccd9b3a77694c1bd85e21c2a0b1cbe6caadb3f8
ian douglas <ian.douglas@iandouglas.com>
2020-03-10 19:53:52 -0600

    added lists, migrations, updated ui

--- todoapp/app.py
+++ todoapp/app.py
@@ -15,15 +15,31 @@ class Todo(db.Model):
   id = db.Column(db.Integer, primary_key=True)
   description = db.Column(db.String, nullable=False)
   completed = db.Column(db.Boolean, default=False, nullable=False)
+  list_id = db.Column(db.Integer, db.ForeignKey('todolists.id'), nullable=False)
 
   def __repr__(self):
     return f'Todo: id:{self.id}, d:{self.description}, c:{self.completed}'
 
-
+class TodoList(db.Model):
+  __tablename__ = 'todolists'
+  id = db.Column(db.Integer, primary_key=True)
+  name = db.Column(db.String, nullable=False)
+  todos = db.relationship('Todo', backref='list', lazy=True)
+  def __repr__(self):
+    return f'TodoList: id:{self.id}, n:{self.name}'
 
 @app.route('/')
 def index():
-  return render_template('index.html', data=Todo.query.order_by('id').all())
+  first_list = TodoList.query.order_by('id').first()
+  return redirect(url_for('list_index', list_id=first_list.id))
+
+@app.route('/lists/<list_id>')
+def list_index(list_id):
+  return render_template('index.html', 
+  lists=TodoList.query.order_by('name').all(),
+  active_list=TodoList.query.get(list_id),
+  todos=Todo.query.filter_by(list_id=list_id).order_by('id').all(),
+)
 
 @app.route('/todos/<todo_id>/set-completed', methods=['POST'])
 def change_todo(todo_id):
@@ -37,25 +53,24 @@ def change_todo(todo_id):
     db.session.close()
   return redirect(url_for('index'))
 
-@app.route('/todos/<todo_id>/delete', methods=['GET'])
+@app.route('/todos/<todo_id>', methods=['DELETE'])
 def delete_todo(todo_id):
   try:
-    todo = Todo.query.get(todo_id)
-    db.session.delete(todo)
+    Todo.query.filter_by(id=todo_id).delete()
     db.session.commit()
   except:
     db.session.rollback()
   finally:
     db.session.close()
-  return redirect(url_for('index'))
+  return jsonify({'success': True})
 
-@app.route('/todos', methods=['POST'])
-def create_todo():
+@app.route('/lists/<list_id>/todos', methods=['POST'])
+def create_todo(list_id):
   error = False
   body = {}
   try:
     description = request.get_json()['description']
-    todo = Todo(description=description)
+    todo = Todo(description=description, list_id=list_id)
     body = {'description': todo.description }
     db.session.add(todo)
     db.session.commit()
