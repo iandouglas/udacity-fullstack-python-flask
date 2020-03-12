54050228a6dd35a8886c1ed2d37d52adf642b9cf
ian douglas <ian.douglas@iandouglas.com>
2020-03-11 22:04:31 -0600

    refactored controllers, dumped database, prepd controllers for fetching db data

---
+++ fyyur/controllers/base.py
@@ -0,0 +1,17 @@
+from flask import render_template
+
+
+def make_routes(app):
+
+    @app.route('/')
+    def index():
+        return render_template('pages/home.html')
+
+    @app.errorhandler(404)
+    def not_found_error(error):
+        return render_template('errors/404.html'), 404
+
+    @app.errorhandler(500)
+    def server_error(error):
+        return render_template('errors/500.html'), 500
+
