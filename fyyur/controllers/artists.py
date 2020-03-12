6698d68590dfdb112b70cc03187c957da160898a
ian douglas <ian.douglas@iandouglas.com>
2020-03-12 01:35:21 -0600

    removed mock data from venue show, successfully populate edit form for artist

--- fyyur/controllers/artists.py
+++ fyyur/controllers/artists.py
@@ -40,22 +40,16 @@ def make_routes(app, db, models):
     @app.route('/artists/<int:artist_id>/edit', methods=['GET'])
     def edit_artist(artist_id):
         form = ArtistForm()
-        # artist = {
-        #     "id": 4,
-        #     "name": "Guns N Petals",
-        #     "genres": ["Rock n Roll"],
-        #     "city": "San Francisco",
-        #     "state": "CA",
-        #     "phone": "326-123-5000",
-        #     "website": "https://www.gunsnpetalsband.com",
-        #     "facebook_link": "https://www.facebook.com/GunsNPetals",
-        #     "seeking_venue": True,
-        #     "seeking_description": "Looking for shows to perform at in the San Francisco Bay Area!",
-        #     "image_link": "https://images.unsplash.com/photo-1549213783-8284d0336c4f?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=300&q=80"
-        # }
-        artist = {}
-        # TODO: populate form with fields from artist with ID <artist_id>
-        return render_template('forms/edit_artist.html', form=form, artist=artist)
+        artist = db.session.query(models['artist']).get(artist_id)
+        if artist:
+            form.name.data = artist.name
+            form.city.data = artist.city
+            form.state.data = artist.state
+            form.phone.data = artist.phone
+            form.genres.data = [genre.name for genre in artist.genres]
+            form.facebook_link.data = artist.facebook_link
+            return render_template('forms/edit_artist.html', form=form, artist=artist)
+        return render_template('errors/404.html')
 
     @app.route('/artists/<int:artist_id>/edit', methods=['POST'])
     def edit_artist_submission(artist_id):
