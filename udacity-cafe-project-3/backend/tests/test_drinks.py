4d100ec4b700c44d819c71196e41e3547eb91373
ian douglas <ian.douglas@iandouglas.com>
2020-09-14 10:20:53 -0600

    added mocked tests for baristas and managers to call the /drinks endpoint

--- udacity-cafe-project-3/backend/tests/test_drinks.py
+++ udacity-cafe-project-3/backend/tests/test_drinks.py
@@ -1,5 +1,7 @@
 import json
 import unittest
+from unittest.mock import patch
+
 from api import create_app, db
 from api.database.models import Drink
 from tests import db_drop_everything, seed_data, assert_payload_field_type_value, assert_payload_field_type
@@ -50,6 +52,99 @@ class GuestUserTest(DrinksTest):
         assert_payload_field_type_value(self, first_recipe, 'parts', int, 1)
 
     def test_endpoint_drinks_happypath_with_no_drinks(self):
+        response = self.client.get('/drinks')
+        data = json.loads(response.data.decode('utf-8'))
+
+        self.assertEqual(200, response.status_code)
+        assert_payload_field_type_value(self, data, 'success', bool, True)
+        assert_payload_field_type_value(self, data, 'drinks', list, [])
+
+
+class BaristaUserTest(DrinksTest):
+    @patch('api.auth.auth.verify_decode_jwt')
+    @patch('api.auth.auth.get_token_auth_header')
+    def test_endpoint_drinks_happypath_with_drinks(self, mock_get_token_auth_header, mock_verify_decode_jwt):
+        mock_get_token_auth_header.return_value = 'barista-token'
+        mock_verify_decode_jwt.return_value = {'permissions': ['get:drinks-detail']}
+        drink = Drink(
+            'blue water',
+            [{
+                "name": "Water",
+                "color": "blue",
+                "parts": 1
+            }]
+        )
+        drink.insert()
+
+        response = self.client.get('/drinks')
+        data = json.loads(response.data.decode('utf-8'))
+
+        self.assertEqual(200, response.status_code)
+        assert_payload_field_type_value(self, data, 'success', bool, True)
+        assert_payload_field_type(self, data, 'drinks', list)
+
+        first_drink = data['drinks'][0]
+        assert_payload_field_type(self, first_drink, 'id', int)
+        assert_payload_field_type_value(self, first_drink, 'title', str, 'blue water')
+        assert_payload_field_type(self, first_drink, 'recipe', list)
+        first_recipe = first_drink['recipe'][0]
+        self.assertIsInstance(first_recipe, dict)
+        assert_payload_field_type_value(self, first_recipe, 'color', str, 'blue')
+        assert_payload_field_type_value(self, first_recipe, 'parts', int, 1)
+
+    @patch('api.auth.auth.verify_decode_jwt')
+    @patch('api.auth.auth.get_token_auth_header')
+    def test_endpoint_drinks_happypath_with_no_drinks(self, mock_get_token_auth_header, mock_verify_decode_jwt):
+        mock_get_token_auth_header.return_value = 'manager-token'
+        mock_verify_decode_jwt.return_value = {'permissions': ['delete:drinks', 'get:drinks-detail', 'patch:drinks', 'post:drinks']}
+
+        response = self.client.get('/drinks')
+        data = json.loads(response.data.decode('utf-8'))
+
+        self.assertEqual(200, response.status_code)
+        assert_payload_field_type_value(self, data, 'success', bool, True)
+        assert_payload_field_type_value(self, data, 'drinks', list, [])
+
+
+class ManagerUserTest(DrinksTest):
+    @patch('api.auth.auth.verify_decode_jwt')
+    @patch('api.auth.auth.get_token_auth_header')
+    def test_endpoint_drinks_happypath_with_drinks(self, mock_get_token_auth_header, mock_verify_decode_jwt):
+        mock_get_token_auth_header.return_value = 'manager-token'
+        mock_verify_decode_jwt.return_value = {'permissions': ['delete:drinks', 'get:drinks-detail', 'patch:drinks', 'post:drinks']}
+        
+        drink = Drink(
+            'blue water',
+            [{
+                "name": "Water",
+                "color": "blue",
+                "parts": 1
+            }]
+        )
+        drink.insert()
+
+        response = self.client.get('/drinks')
+        data = json.loads(response.data.decode('utf-8'))
+
+        self.assertEqual(200, response.status_code)
+        assert_payload_field_type_value(self, data, 'success', bool, True)
+        assert_payload_field_type(self, data, 'drinks', list)
+
+        first_drink = data['drinks'][0]
+        assert_payload_field_type(self, first_drink, 'id', int)
+        assert_payload_field_type_value(self, first_drink, 'title', str, 'blue water')
+        assert_payload_field_type(self, first_drink, 'recipe', list)
+        first_recipe = first_drink['recipe'][0]
+        self.assertIsInstance(first_recipe, dict)
+        assert_payload_field_type_value(self, first_recipe, 'color', str, 'blue')
+        assert_payload_field_type_value(self, first_recipe, 'parts', int, 1)
+
+    @patch('api.auth.auth.verify_decode_jwt')
+    @patch('api.auth.auth.get_token_auth_header')
+    def test_endpoint_drinks_happypath_with_no_drinks(self, mock_get_token_auth_header, mock_verify_decode_jwt):
+        mock_get_token_auth_header.return_value = 'barista-token'
+        mock_verify_decode_jwt.return_value = {'permissions': ['get:drinks-detail']}
+
         response = self.client.get('/drinks')
         data = json.loads(response.data.decode('utf-8'))
 
